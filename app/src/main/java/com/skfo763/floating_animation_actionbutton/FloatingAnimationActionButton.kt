b8d7dc2d42b3205e5fb795263fc9129b08484ff5
skfo763 <scy7351@naver.com>
2020-02-21 22:30:29 +0900

    Typed Array 속성 받아오는 부분 추가

--- app/src/main/java/com/skfo763/floating_animation_actionbutton/FloatingAnimationActionButton.kt
+++ app/src/main/java/com/skfo763/floating_animation_actionbutton/FloatingAnimationActionButton.kt
@@ -2,6 +2,8 @@ package com.skfo763.floating_animation_actionbutton
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -9,6 +11,7 @@ import android.view.View
 import androidx.appcompat.view.SupportMenuInflater
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.internal.ViewUtils
 
@@ -16,13 +19,22 @@ class FloatingAnimationActionButton
 @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0):
         ConstraintLayout(context, attrs, defStyle) {
 
-    private var _isOpened = false
-    var isOpened: Boolean get() = _isOpened
-
     private var selectedListener: OnSubButtonClickListener? = null
     private var menu: MenuBuilder = FloatingSubActionButtonMenu(context)
-
     private var menuInflater: MenuInflater? = null
+
+    private var iconRes: Drawable? = null
+    private var btnColor = Color.parseColor("#AAAAAA")
+    private var buttonSize: Float
+    private var iconSize: Float
+    private var subButtonSize: Float
+    private var subButtonMargin: Float
+    private var subButtonColor = Color.parseColor("#654321")
+    private var subButtonIconSize: Float
+    private var _isOpened = false
+
+
+    var isOpened: Boolean get() = _isOpened
 
     private val subItemMenuCallback = object: MenuBuilder.Callback {
         override fun onMenuItemSelected(menu: MenuBuilder?, item: MenuItem?): Boolean {
@@ -39,6 +51,15 @@ class FloatingAnimationActionButton
         val typedArray = context.theme.obtainStyledAttributes(
             attrs, R.styleable.FloatingAnimationActionButton, 0, 0)
         try {
+            iconRes = typedArray.getDrawable(R.styleable.FloatingAnimationActionButton_icon)
+            iconSize = typedArray.getDimension(R.styleable.FloatingAnimationActionButton_iconSize, R.dimen.fab_main_icon_size.toFloat())
+            btnColor = typedArray.getColor(R.styleable.FloatingAnimationActionButton_buttonBackgroundColor, 0xAAAAAA)
+            buttonSize = typedArray.getDimension(R.styleable.FloatingAnimationActionButton_buttonSize, R.dimen.fab_main_button_margin_top.toFloat())
+            subButtonSize = typedArray.getDimension(R.styleable.FloatingAnimationActionButton_subButtonSize, R.dimen.fab_sub_button_size.toFloat())
+            subButtonColor = typedArray.getColor(R.styleable.FloatingAnimationActionButton_subButtonColor, 0x654321)
+            subButtonMargin = typedArray.getDimension(R.styleable.FloatingAnimationActionButton_subButtonMargin, R.dimen.fab_sub_button_margin.toFloat())
+            subButtonIconSize = typedArray.getDimension(R.styleable.FloatingAnimationActionButton_subButtonIconSize, R.dimen.fab_sub_icon_size.toFloat())
+
             isOpened = typedArray.getBoolean(R.styleable.FloatingAnimationActionButton_isOpened, false)
         } finally {
             typedArray.recycle()
